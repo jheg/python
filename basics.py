fbdf59acd1a1b69f407208224b7b983513b20ef2
Jason Hegarty <jasonhegarty@jasons-mbp.home>
2023-06-20 20:56:16 +0100

    Initial commit

---
+++ basics.py
@@ -0,0 +1,16 @@
+# visitors_name = input('please tell us your name ')
+# print(f"Hello {visitors_name}")
+
+# Lesson 7: Functions (Part 2)
+def calculate_power(base, exponent):
+    result = base ** exponent
+    print(result)
+
+calculate_power(2,3)
+
+# Lesson 8: Lists
+numbers = [1,2,3]
+print(numbers[1])
+numbers[0] = 4
+numbers.append(5)
+print(numbers)
